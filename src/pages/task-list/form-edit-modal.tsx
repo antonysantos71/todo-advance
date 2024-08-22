5ab6f8a287d83c3df5e802785c9a2c0a5dab8ae1
antonysantos71 <ribeiroantony02.56@gmail.com>
2024-08-21 22:14:04 -0300

    feat: Added edit and delete options for tasks

--- src/pages/task-list/form-edit-modal.tsx
+++ src/pages/task-list/form-edit-modal.tsx
@@ -1,25 +1,52 @@
-import { X } from "lucide-react";
+import React, { useState, useEffect } from 'react';
+import type { IListPrps } from '.';
+import { X } from 'lucide-react';
 
-interface IFormCreateModalProps {
-  closeCreateModal: () => void;
-  createTask: () => void;
-  setTitle: (value: string) => void;
-  setDescription: (value: string) => void;
+interface FormEditModalProps {
+  closeEditModal: () => void;
+  task: IListPrps;
+  updateTask: (id: number, newTask: Omit<IListPrps, "id">) => Promise<void>;
 }
 
-export const FormCreateModal = ({
-  closeCreateModal,
-  createTask,
-  setTitle,
-  setDescription,
-}: IFormCreateModalProps) => {
+export const FormEditModal: React.FC<FormEditModalProps> = ({
+  closeEditModal,
+  task,
+  updateTask,
+}) => {
+  const [title, setTitle] = useState<string>(task.title);
+  const [description, setDescription] = useState<string>(task.description);
+
+  useEffect(() => {
+    setTitle(task.title);
+    setDescription(task.description);
+  }, [task]);
+
+  const updateTaskFn = async () => {
+    if (title === "" || description === "") {
+      alert('Necessario preecher todos os campos');
+      return;
+    } else {
+      try {
+        await updateTask(task.id, {
+          title,
+          description,
+          dateCreated: task.dateCreated
+        });
+        closeEditModal()
+      } catch (error) {
+        console.error('Error updating task:', error);
+      } 
+    }
+   
+  };
+
   return (
     <div className="fixed inset-0 flex justify-center items-center min-h-screen bg-black bg-opacity-50 z-50">
       <div className="bg-zinc-800 p-8 rounded-lg shadow-lg w-full max-w-sm">
         <form onSubmit={(e) => e.preventDefault()} className="space-y-4">
           <div className="flex justify-between items-center">
             <span className="text-lg">create new tasks</span>
-            <button onClick={closeCreateModal}>
+            <button onClick={closeEditModal}>
               <X className="size-5" />
             </button>
           </div>
@@ -30,6 +57,7 @@ export const FormCreateModal = ({
             <input
               type="text"
               className="outline-0 mt-1 border-b-0.5 block w-full bg-transparent  shadow-sm "
+              value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
           </div>
@@ -40,17 +68,17 @@ export const FormCreateModal = ({
             <input
               type="text"
               className="outline-0 mt-1 border-b-0.5 block w-full bg-transparent  shadow-sm "
+              value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
           </div>
           <button
             onClick={() => {
-              createTask();
-              closeCreateModal();
+              updateTaskFn();
             }}
             className="w-full py-2 px-4 bg-zinc-500 text-white font-semibold rounded-md shadow-md hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           >
-            create
+            edita
           </button>
         </form>
       </div>
--- src/pages/task-list/index.tsx
+++ src/pages/task-list/index.tsx
@@ -3,13 +3,13 @@ import { Header } from "../../components/header";
 import { Aside } from "../../components/aside";
 import { FormCreateModal } from "./form-create-modal";
 import { taskServices } from "../../services/api/tasks-services";
-import { Link2 } from "lucide-react";
+import { Edit, Trash } from "lucide-react";
+import { FormEditModal } from "./form-edit-modal";
 
-interface IListPrps {
+export interface IListPrps {
   id: number;
   title: string;
   description: string;
-  completed: boolean;
   dateCreated: string;
 }
 
@@ -18,13 +18,22 @@ export const TaskList = () => {
   const [list, setList] = useState<IListPrps[]>([]);
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
+  const [editTask, setEditTask] = useState<IListPrps | null>(null);
+  const [editModal, setEditModal] = useState<boolean>(false);
 
-  function openCreateModal() {
-    setModal(true);
-  }
-  function closeCreateModal() {
-    setModal(false);
-  }
+  const openCreateModal = () => setModal(true);
+
+  const closeCreateModal = () => setModal(false);
+
+  const openEditModal = (task: IListPrps) => {
+    setEditTask(task);
+    setEditModal(true);
+  };
+
+  const closeEditModal = () => {
+    setEditTask(null);
+    setEditModal(false);
+  };
 
   useEffect(() => {
     const getTasks = async () => {
@@ -40,21 +49,51 @@ export const TaskList = () => {
   const createTaks = async () => {
     try {
       const newTask = {
-        id: Date.now(),
+        // id: Date.now(),
         title,
         description,
-        completed: false,
         dateCreated: new Date().toISOString(),
       };
-      const createdTasks = await taskServices.createTask(newTask);
-      setList((previusTaks) => [...previusTaks, createdTasks]);
+      if (title === "") {
+        alert("Title is required");
+        return;
+      } else {
+        const createdTasks = await taskServices.createTask(newTask);
+        setList((previusTaks) => [...previusTaks, createdTasks]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateTask = async (
+    id: number,
+    newTask: Omit<IListPrps, "id">
+  ): Promise<void> => {
+    try {
+      await taskServices.updateTask(id, newTask);
+      setList((previousTasks) =>
+        previousTasks.map((task) =>
+          task.id === id ? { id, ...newTask } : task
+        )
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTask = async (id: number) => {
+    try {
+      await taskServices.deleteTask(id);
+      setList((previousTasks) =>
+        previousTasks.filter((task) => task.id !== id)
+      );
     } catch (error) {
       console.error(error);
     }
   };
 
   const [showAll, setShowAll] = useState(false);
-
   const initialDisplayCount = 4;
 
   return (
@@ -73,18 +112,28 @@ export const TaskList = () => {
             />
           )}
 
+          {editModal && editTask && (
+            <FormEditModal
+              closeEditModal={closeEditModal}
+              task={editTask}
+              updateTask={updateTask}
+            />
+          )}
+
           <div className="task-container max-h-96 overflow-y-auto px-12 my-12">
             {list.length === 0 ? (
               <p className="text-sm text-gray-500">
                 Nenhuma atividade cadastrada.
               </p>
-            ): ""}
+            ) : (
+              ""
+            )}
             {list
               .slice(0, showAll ? list.length : initialDisplayCount)
               .map((task) => (
                 <div
                   key={task.id}
-                  className="flex justify-between bg-zinc-800 p-3 px-5 rounded-md mb-2"
+                  className="flex justify-between bg-zinc-800 p-3 px-5 rounded-md mb-2 flex-wrap"
                 >
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
@@ -94,11 +143,22 @@ export const TaskList = () => {
                       {task.description}
                     </span>
                   </div>
-                  <div className="flex items-center">
-                    <button className="flex gap-2 items-center text-blue-500">
-                      view task
-                      {/* Supondo que Link2 é um componente de ícone */}
-                      <Link2 className="size-5" />
+                  <div className="flex items-center justify-center gap-3">
+                    <button
+                      onClick={() => {
+                        openEditModal(task);
+                      }}
+                      className="flex text-blue-500"
+                    >
+                      <Edit />
+                    </button>
+                    <button
+                      onClick={() => {
+                        deleteTask(task.id);
+                      }}
+                      className="flex text-blue-500"
+                    >
+                      <Trash />
                     </button>
                   </div>
                 </div>
